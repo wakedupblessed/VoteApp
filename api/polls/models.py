c2f8c337c59ed778598d191bd2f1135a45d90ede
KirillSokirka <k.sokirka-2024@kpi.ua>
2023-03-10 11:32:48 +0200

    add models for basic application entities

--- api/polls/models.py
+++ api/polls/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=300)
+    number_of_vote = models.IntegerField(default=0)
+    creation_date = models.DateTimeField()
+    is_anonymous = models.BooleanField(default=False)
+
+    voters = models.ManyToManyField(User, through='PollVoter')
+    tags = models.ManyToManyField('polls.Tag', through='PollTag')
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
+
+
+class PollVoter(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.RESTRICT)
+    voter = models.ForeignKey(User, on_delete=models.RESTRICT)
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=80)
+
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class Option(models.Model):
+    name = models.CharField(max_length=40)
+    number_of_vote = models.IntegerField(default=0)
+    is_correct = models.BooleanField(default=False)
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class PollTag(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.RESTRICT)
+    tag = models.ForeignKey(Tag, on_delete=models.RESTRICT)
+
+
+
