93d70183ec552c67388d74b8542cd478d94f8666
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2023-05-08 19:13:35 +0300

    styled login and register page

--- web/src/pages/LoginPage/LoginPage.tsx
+++ web/src/pages/LoginPage/LoginPage.tsx
@@ -1,17 +1,55 @@
 import { useContext } from "react";
 import AuthContext from "../../сontext/AuthContext";
 import React from "react";
+import styled from "styled-components";
 
 const LoginPage = () => {
   const { loginUser } = useContext(AuthContext)!;
 
   return (
-    <form onSubmit={loginUser}>
-      <input type='text' name='username' placeholder='Enter username' />
-      <input type='password' name='password' placeholder='Enter password' />
-      <input type='submit' />
-    </form>
+    <LoginFormContainer onSubmit={loginUser}>
+      <LoginFormElement>
+      <label htmlFor="username">Username</label>
+      <StyledInput type="text" name="username" placeholder="Enter username" />
+      </LoginFormElement>
+      <LoginFormElement>
+      <label htmlFor="username">Username</label>
+      <StyledInput type="password" name="password" placeholder="Enter password" />
+      </LoginFormElement>
+      <StyledLoginButton type="submit">Submit</StyledLoginButton>
+    </LoginFormContainer>
   );
 };
 
 export default LoginPage;
+
+const StyledInput = styled.input`
+  width: 200px;
+  font-size: 16px;
+`;
+
+const StyledLoginButton = styled.button`
+  width: fit-content;
+
+  padding: 12px 20px;
+  border-radius: 40px;
+  font-size: 14px;
+  color: #fff;
+  font-weight: normal;
+  background-color: #000;
+  line-height: 16px;
+  border: none;
+`;
+
+const LoginFormElement = styled.div`
+  font-size: 18px;
+  width: 200px;
+`;
+
+const LoginFormContainer = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
--- web/src/pages/RegisterPage/RegisterPage.tsx
+++ web/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,26 +1,59 @@
 import { useContext } from "react";
 import AuthContext from "../../сontext/AuthContext";
+import styled from "styled-components";
 
 const RegisterPage = () => {
   const { registerUser } = useContext(AuthContext)!;
 
   return (
-    <form onSubmit={registerUser}>
-      <div>
-        <label htmlFor='username'>Username</label>
-        <input type='text' name='username' required />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input type='password' name='password' required />
-      </div>
-      <div>
-        <label htmlFor='confirmPassword'>Confirm Password</label>
-        <input type='password' name='confirmPassword' required />
-      </div>
-      <button type='submit'>Register</button>
-    </form>
+    <RegisterFormContainer onSubmit={registerUser}>
+      <RegisterFormElement>
+        <label htmlFor="username">Username</label>
+        <StyledInput type="text" name="username" required />
+      </RegisterFormElement>
+      <RegisterFormElement>
+        <label htmlFor="password">Password</label>
+        <StyledInput type="password" name="password" required />
+      </RegisterFormElement>
+      <RegisterFormElement>
+        <label htmlFor="confirmPassword">Confirm password</label>
+        <StyledInput type="password" name="confirmPassword" required />
+      </RegisterFormElement>
+      <StyledRegisterButton type="submit">Register</StyledRegisterButton>
+    </RegisterFormContainer>
   );
 };
 
 export default RegisterPage;
+
+const StyledInput= styled.input`
+  width: 200px;
+  font-size:16px;
+`;
+
+
+const StyledRegisterButton = styled.button`
+  width: fit-content;
+
+  padding: 12px 20px;
+  border-radius: 40px;
+  font-size: 14px;
+  color: #fff;
+  font-weight: normal;
+  background-color: #000;
+  line-height: 16px;
+  border: none;
+`;
+
+const RegisterFormElement = styled.div`
+font-size: 18px;
+  width: 200px;
+`;
+
+const RegisterFormContainer = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
